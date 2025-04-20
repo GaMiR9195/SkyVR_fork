9662243f80d88790c112d25458ebc1f8d4cd3967
diswcord <109433110+GaMiR9195@users.noreply.github.com>
2025-04-21 02:33:47 +0300

    Update skyfork_hatdrop.lua

--- skyfork_hatdrop.lua
+++ skyfork_hatdrop.lua
@@ -1,135 +1,231 @@
--- drops all ur hats wherever you ran the script
--- made as a proof of concept and resource for how to use the hats (animations, fling, whatever) for your own script
-local fph = workspace.FallenPartsDestroyHeight
-
-local plr = game.Players.LocalPlayer
-local character = plr.Character
-local hrp = character:WaitForChild("HumanoidRootPart")
-local torso = character:FindFirstChild("UpperTorso") or character:FindFirstChild("Torso")
-local start = hrp.CFrame
-
-local campart = Instance.new("Part",character)
-campart.Transparency = 1
-campart.CanCollide = false
-campart.Size = Vector3.one
-campart.Position = start.Position
-campart.Anchored = true
-
-local function updatestate(hat,state)
-    if sethiddenproperty then
-        sethiddenproperty(hat,"BackendAccoutrementState",state)
-    elseif setscriptable then
-        setscriptable(hat,"BackendAccoutrementState",true)
-        hat.BackendAccoutrementState = state
-    else
-        local success = pcall(function()
-            hat.BackendAccoutrementState = state
-        end)
-        if not success then
-            error("executor not supported, sorry!")
-        end
-    end
+local function updatestate(hat, state)
+	if sethiddenproperty then
+		sethiddenproperty(hat, "BackendAccoutrementState", state)
+	elseif setscriptable then
+		setscriptable(hat, "BackendAccoutrementState", true)
+		hat.BackendAccoutrementState = state
+	else
+		pcall(function()
+			hat.BackendAccoutrementState = state
+		end)
+	end
 end
 
-local allhats = {}
-for i,v in pairs(character:GetChildren()) do
-    if v:IsA("Accessory") then
-        table.insert(allhats,v)
-    end
+local function performHatDrop(character, callback)
+	local fph = workspace.FallenPartsDestroyHeight
+	local hrp = character:WaitForChild("HumanoidRootPart")
+	local torso = character:FindFirstChild("UpperTorso") or character:FindFirstChild("Torso")
+
+	local allhats = {}
+	for _, acc in pairs(character:GetChildren()) do
+		if acc:IsA("Accessory") then
+			table.insert(allhats, acc)
+		end
+	end
+
+	local locks = {}
+	for _, hat in pairs(allhats) do
+		table.insert(locks, hat.Changed:Connect(function(p)
+			if p == "BackendAccoutrementState" then
+				updatestate(hat, 0)
+			end
+		end))
+		updatestate(hat, 2)
+	end
+
+	workspace.FallenPartsDestroyHeight = 0/0
+	local dropcf = CFrame.new(hrp.Position.X, fph - .25, hrp.Position.Z)
+	if character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
+		dropcf *= CFrame.Angles(math.rad(20), 0, 0)
+		character.Humanoid:ChangeState(16)
+		local r15fall = 507767968
+		local anim = Instance.new("Animation")
+		anim.AnimationId = "rbxassetid://" .. tostring(r15fall)
+		local track = character.Humanoid:LoadAnimation(anim)
+		track.Priority = Enum.AnimationPriority.Action
+		track:Play()
+		track:AdjustSpeed(1)
+		track.TimePosition = .1
+	else
+		local r6fall = 180436148
+		local anim = Instance.new("Animation")
+		anim.AnimationId = "rbxassetid://" .. tostring(r6fall)
+		local track = character.Humanoid:LoadAnimation(anim)
+		track.Priority = Enum.AnimationPriority.Action
+		track:Play()
+		track:AdjustSpeed(1)
+		track.TimePosition = .1
+	end
+
+	spawn(function()
+		while hrp.Parent do
+			hrp.CFrame = dropcf
+			hrp.Velocity = Vector3.new(0, 25, 0)
+			hrp.RotVelocity = Vector3.new(0, 0, 0)
+			game:GetService("RunService").Heartbeat:Wait()
+		end
+	end)
+
+	task.wait(0.25)
+	character.Humanoid:ChangeState(15)
+	torso.AncestryChanged:Wait()
+
+	for _, con in pairs(locks) do
+		con:Disconnect()
+	end
+	for _, hat in pairs(allhats) do
+		updatestate(hat, 4)
+	end
+
+	-- Get all hats and handle them
+	local function getAllHats(c)
+		local list = {}
+		local foundmeshids = {}
+		for i, v in pairs(c:GetChildren()) do
+			if not v:IsA("Accessory") then continue end
+			if not v.Handle:FindFirstChildOfClass("SpecialMesh") then continue end
+			local meshId = filterMeshID(v.Handle:FindFirstChildOfClass("SpecialMesh").MeshId)
+			local found, type = findMeshID(meshId)
+			if foundmeshids["meshid:"..meshId] then
+				found = false
+			else
+				foundmeshids["meshid:"..meshId] = true
+			end
+			if found then
+				table.insert(list, {v, type, "meshid:"..meshId})
+			else
+				local named, type = findHatName(v.Name)
+				if named then
+					table.insert(list, {v, type, v.Name})
+				end
+			end
+		end
+		return list
+	end
+
+	local allhats = getAllHats(character)
+	callback(allhats)
 end
 
-local locks = {}
-for i,v in pairs(allhats) do
-    table.insert(locks,v.Changed:Connect(function(p)
-        if p == "BackendAccoutrementState" then
-            updatestate(v,0)
-        end
-    end))
-    updatestate(v,2)
+-- The rest of the script
+
+local function createpart(size, name, h)
+	local Part = Instance.new("Part")
+	Part.Parent = workspace
+	Part.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+	Part.Size = size
+	Part.Transparency = 1
+	Part.CanCollide = false
+	Part.Anchored = true
+	Part.Name = name
+	return Part
 end
 
-workspace.FallenPartsDestroyHeight = 0/0
+local ps = game:GetService("RunService").PostSimulation
+local input = game:GetService("UserInputService")
+local fpdh = game.Workspace.FallenPartsDestroyHeight
+local Player = game.Players.LocalPlayer
+local options = getgenv().options
 
-local function play(id,speed,prio,weight)
-    local Anim = Instance.new("Animation")
-    Anim.AnimationId = "https"..tostring(math.random(1000000,9999999)).."="..tostring(id)
-    local track = character.Humanoid:LoadAnimation(Anim)
-    track.Priority = prio
-    track:Play()
-    track:AdjustSpeed(speed)
-    track:AdjustWeight(weight)
-    return track
+local lefthandpart = createpart(Vector3.new(2,1,1), "moveRH", true)
+local righthandpart = createpart(Vector3.new(2,1,1), "moveRH", true)
+local headpart = createpart(Vector3.new(1,1,1), "moveH", false)
+local lefttoypart = createpart(Vector3.new(1,1,1), "LToy", true)
+local righttoypart = createpart(Vector3.new(1,1,1), "RToy", true)
+local thirdperson = false
+local lefttoyenable = false
+local righttoyenable = false
+local lfirst = true
+local rfirst = true
+local ltoypos = CFrame.new(1.15,0,0) * CFrame.Angles(0,math.rad(180),0)
+local rtoypos = CFrame.new(1.15,0,0) * CFrame.Angles(0,math.rad(0),0)
+local parts = {
+	left = lefthandpart,
+	right = righthandpart,
+	headhats = headpart,
+	leftToy = lefttoypart,
+	rightToy = righttoypart,
+}
+
+function _isnetworkowner(Part)
+	return Part.ReceiveAge == 0
 end
 
-local r6fall = 180436148
-local r15fall = 507767968
+game.Workspace.FallenPartsDestroyHeight = 0/0
 
-local dropcf = CFrame.new(character.HumanoidRootPart.Position.x,fph-.25,character.HumanoidRootPart.Position.z)
-if character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
-    dropcf =  dropcf * CFrame.Angles(math.rad(20),0,0)
-    character.Humanoid:ChangeState(16)
-    play(r15fall,1,5,1).TimePosition = .1
-else
-    play(r6fall,1,5,1).TimePosition = .1
+function filterMeshID(id)
+	return (string.find(id,'assetdelivery') ~= nil and string.match(string.sub(id,37, #id), "%d+")) or string.match(id, "%d+")
 end
 
-spawn(function()
-    while hrp.Parent ~= nil do
-        hrp.CFrame = dropcf
-        hrp.Velocity = Vector3.new(0,25,0)
-        hrp.RotVelocity = Vector3.new(0,0,0)
-        game:GetService("RunService").Heartbeat:wait()
-    end
+function findMeshID(id)
+	for i, v in pairs(getgenv().headhats) do
+		if i == "meshid:" .. id then return true, "headhats" end
+	end
+	if getgenv().right == "meshid:" .. id then return true, "right" end
+	if getgenv().left == "meshid:" .. id then return true, "left" end
+	if options.leftToy == "meshid:" .. id then return true, "leftToy" end
+	if options.rightToy == "meshid:" .. id then return true, "rightToy" end
+	return false
+end
+
+function findHatName(id)
+	for i, v in pairs(getgenv().headhats) do
+		if i == id then return true, "headhats" end
+	end
+	if getgenv().right == id then return true, "right" end
+	if getgenv().left == id then return true, "left" end
+	if options.leftToy == id then return true, "leftToy" end
+	if options.rightToy == id then return true, "rightToy" end
+	return false
+end
+
+function Align(Part1, Part0, cf, isflingpart)
+	local up = isflingpart
+	local con
+	con = ps:Connect(function()
+		if up ~= nil then up = not up end
+		if not Part1:IsDescendantOf(workspace) then con:Disconnect() return end
+		if not _isnetworkowner(Part1) then return end
+		Part1.CanCollide = false
+		Part1.CFrame = Part0.CFrame * cf
+		Part1.Velocity = velocity or Vector3.new(20, 20, 20)
+	end)
+
+	return {
+		SetVelocity = function(self, v) velocity = v end,
+		SetCFrame = function(self, v) cf = v end,
+	}
+end
+
+game:GetService("StarterGui"):SetCore("VREnableControllerModels", false)
+
+local rightarmalign = nil
+getgenv().con5 = input.UserCFrameChanged:connect(function(part, move)
+	cam.CameraType = "Scriptable"
+	cam.HeadScale = options.headscale
+	if part == Enum.UserCFrame.Head then
+		headpart.CFrame = cam.CFrame * (CFrame.new(move.p * (cam.HeadScale - 1)) * move)
+	elseif part == Enum.UserCFrame.LeftHand then
+		lefthandpart.CFrame = cam.CFrame * (CFrame.new(move.p * (cam.HeadScale - 1)) * move * CFrame.Angles(math.rad(options.lefthandrotoffset.X), math.rad(options.lefthandrotoffset.Y), math.rad(options.lefthandrotoffset.Z)))
+		if lefttoyenable then
+			lefttoypart.CFrame = lefthandpart.CFrame * ltoypos
+		end
+	elseif part == Enum.UserCFrame.RightHand then
+		righthandpart.CFrame = cam.CFrame * (CFrame.new(move.p * (cam.HeadScale - 1)) * move * CFrame.Angles(math.rad(options.righthandrotoffset.X), math.rad(options.righthandrotoffset.Y), math.rad(options.righthandrotoffset.Z)))
+		if righttoyenable then
+			righttoypart.CFrame = righthandpart.CFrame * rtoypos
+		end
+	end
 end)
 
-task.wait(.25)
-character.Humanoid:ChangeState(15)
-torso.AncestryChanged:wait()
+getgenv().conn = Player.CharacterAdded:Connect(function(Character)
+	performHatDrop(Character, function(allhats)
+		for i, v in pairs(allhats) do
+			if not v[1]:FindFirstChild("Handle") then continue end
+			if v[2] == "headhats" then v[1].Handle.Transparency = options.HeadHatTransparency or 1 end
 
-for i,v in pairs(locks) do
-    v:Disconnect()
-end
-for i,v in pairs(allhats) do
-    updatestate(v,4)
-end
-
-spawn(function()
-    plr.CharacterAdded:wait():WaitForChild("HumanoidRootPart",10).CFrame = start
-    workspace.FallenPartsDestroyHeight = fph
+			local align = Align(v[1].Handle, parts[v[2]], ((v[2] == "headhats") and getgenv()[v[2]][v[3]]) or CFrame.identity)
+			rightarmalign = v[2] == "right" and align or rightarmalign
+		end
+	end)
 end)
-
-local dropped = false
-repeat
-    local foundhandle = false
-    for i,v in pairs(allhats) do
-        if v:FindFirstChild("Handle") then
-            foundhandle = true
-            if v.Handle.CanCollide then
-                dropped = true
-                break
-            end
-        end
-    end
-    if not foundhandle then
-        break
-    end
-    task.wait()
-until plr.Character ~= character or dropped
-
-if dropped then
-    print("dropped")
-    workspace.CurrentCamera.CameraSubject = campart
-    for i,v in pairs(character:GetChildren()) do
-        if v:IsA("Accessory") and v:FindFirstChild("Handle") and v.Handle.CanCollide then
-            spawn(function()
-                for i = 1,10 do
-                    v.Handle.CFrame = start
-                    v.Handle.Velocity = Vector3.new(0,50,0)
-                    task.wait()
-                end
-            end)
-        end
-    end
-else
-    print("failed to drop")
-end
