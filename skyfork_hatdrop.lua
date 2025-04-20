39908298eb02076379712928e185f7c0828dc42c
diswcord <109433110+GaMiR9195@users.noreply.github.com>
2025-04-21 02:26:40 +0300

    Update skyfork_hatdrop.lua

--- skyfork_hatdrop.lua
+++ skyfork_hatdrop.lua
@@ -1,14 +1,15 @@
 function HatdropCallback(Character, callback)
     local fph = workspace.FallenPartsDestroyHeight
+    local plr = game.Players.LocalPlayer
     local hrp = Character:WaitForChild("HumanoidRootPart")
     local torso = Character:FindFirstChild("UpperTorso") or Character:FindFirstChild("Torso")
-    local startCF = hrp.CFrame
+    local start = hrp.CFrame
 
     local campart = Instance.new("Part", Character)
     campart.Transparency = 1
     campart.CanCollide = false
     campart.Size = Vector3.one
-    campart.Position = startCF.Position
+    campart.Position = start.Position
     campart.Anchored = true
 
     local function updatestate(hat, state)
@@ -22,48 +23,51 @@ function HatdropCallback(Character, callback)
                 hat.BackendAccoutrementState = state
             end)
             if not success then
-                warn("Executor not supported!")
+                error("executor not supported, sorry!")
             end
         end
     end
 
     local allhats = {}
-    for _, hat in ipairs(Character:GetChildren()) do
-        if hat:IsA("Accessory") then
-            table.insert(allhats, hat)
+    for i,v in pairs(Character:GetChildren()) do
+        if v:IsA("Accessory") then
+            table.insert(allhats, v)
         end
     end
 
     local locks = {}
-    for _, hat in ipairs(allhats) do
-        table.insert(locks, hat.Changed:Connect(function(prop)
-            if prop == "BackendAccoutrementState" then
-                updatestate(hat, 0)
+    for i,v in pairs(allhats) do
+        table.insert(locks, v.Changed:Connect(function(p)
+            if p == "BackendAccoutrementState" then
+                updatestate(v, 0)
             end
         end))
-        updatestate(hat, 2)
+        updatestate(v, 2)
     end
 
-    workspace.FallenPartsDestroyHeight = 0 / 0
+    workspace.FallenPartsDestroyHeight = 0/0
+
+    local function play(id, speed, prio, weight)
+        local Anim = Instance.new("Animation")
+        Anim.AnimationId = "rbxassetid://" .. tostring(id)
+        local track = Character.Humanoid:LoadAnimation(Anim)
+        track.Priority = prio
+        track:Play()
+        track:AdjustSpeed(speed)
+        track:AdjustWeight(weight)
+        return track
+    end
 
     local r6fall = 180436148
     local r15fall = 507767968
 
-    local dropcf = CFrame.new(hrp.Position.X, fph - 0.25, hrp.Position.Z)
+    local dropcf = CFrame.new(hrp.Position.x, fph - .25, hrp.Position.z)
     if Character.Humanoid.RigType == Enum.HumanoidRigType.R15 then
         dropcf = dropcf * CFrame.Angles(math.rad(20), 0, 0)
         Character.Humanoid:ChangeState(16)
-        local r15 = Instance.new("Animation")
-        r15.AnimationId = "rbxassetid://" .. r15fall
-        local track = Character.Humanoid:LoadAnimation(r15)
-        track:Play()
-        track.TimePosition = 0.1
+        play(r15fall, 1, 5, 1).TimePosition = .1
     else
-        local r6 = Instance.new("Animation")
-        r6.AnimationId = "rbxassetid://" .. r6fall
-        local track = Character.Humanoid:LoadAnimation(r6)
-        track:Play()
-        track.TimePosition = 0.1
+        play(r6fall, 1, 5, 1).TimePosition = .1
     end
 
     spawn(function()
@@ -79,17 +83,54 @@ function HatdropCallback(Character, callback)
     Character.Humanoid:ChangeState(15)
     torso.AncestryChanged:Wait()
 
-    for _, conn in ipairs(locks) do
-        conn:Disconnect()
+    for i,v in pairs(locks) do
+        v:Disconnect()
     end
-    for _, hat in ipairs(allhats) do
-        updatestate(hat, 4)
+    for i,v in pairs(allhats) do
+        updatestate(v, 4)
     end
 
     spawn(function()
-        game.Players.LocalPlayer.CharacterAdded:Wait():WaitForChild("HumanoidRootPart", 10).CFrame = startCF
+        plr.CharacterAdded:Wait():WaitForChild("HumanoidRootPart", 10).CFrame = start
         workspace.FallenPartsDestroyHeight = fph
     end)
 
+    local dropped = false
+    repeat
+        local foundhandle = false
+        for i,v in pairs(allhats) do
+            if v:FindFirstChild("Handle") then
+                foundhandle = true
+                if v.Handle.CanCollide then
+                    dropped = true
+                    break
+                end
+            end
+        end
+        if not foundhandle then
+            break
+        end
+        task.wait()
+    until plr.Character ~= Character or dropped
+
+    if dropped then
+        print("dropped")
+        workspace.CurrentCamera.CameraSubject = campart
+        for i,v in pairs(Character:GetChildren()) do
+            if v:IsA("Accessory") and v:FindFirstChild("Handle") and v.Handle.CanCollide then
+                spawn(function()
+                    for i = 1, 10 do
+                        v.Handle.CFrame = start
+                        v.Handle.Velocity = Vector3.new(0, 50, 0)
+                        task.wait()
+                    end
+                end)
+            end
+        end
+    else
+        print("failed to drop")
+    end
+
+    -- Send collected hat info to callback (keep integration)
     callback(getAllHats(Character))
 end
